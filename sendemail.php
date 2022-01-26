030f2ccd939195f7ea12fb50f8e2ea73add69efd
Tharaka901 <tharakamihiri@gmail.com>
2022-01-26 09:50:27 +0530

    mail

---
+++ sendemail.php
@@ -0,0 +1,27 @@
+<?php
+    require "vendor/autoload.php";
+
+    class SendEmail{
+
+        public static function SendMail($to,$subject,$content){
+            $key="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";
+
+            $email = new \SendGrid\Mail\Mail();
+            $email->setFrom("tharakamihiri@gmail.com", "Tharaka Mihiri");
+            $email->setSubject($subject);
+            $email->addTo($to);
+            $email->addContent("text/plain", $content);
+
+            $sendgrid = new \SendGrid($key);  
+
+            try{
+                $response = $sendgrid->send($email);
+                return $response;
+            }catch(Exception $e){
+                echo "Email exeption Caught : ".$e->getMessage() . "\n";
+                return false;
+            }
+
+        }
+    }
+?> 
